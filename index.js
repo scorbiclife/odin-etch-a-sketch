055c29a32d9de3257a8fa6c98e4c5736d53adfbb
scorbiclife <scorbiclife+git@gmail.com>
2025-03-17 15:14:44 +0900

    feat(js): randomize colors on hover

--- index.js
+++ index.js
@@ -21,7 +21,27 @@ const mainContainer = document.querySelector(".main-container");
 const MAIN_CONTAINER_WIDTH_PX = 320
 mainContainer.style.width = `${MAIN_CONTAINER_WIDTH_PX}px`
 
+let nHovers = 0;
+
+function resetHoverCount() {
+  nHovers = 0;
+}
+
+function incrementHoverCount() {
+  ++nHovers;
+}
+
+function getOpacityValue() {
+  return `${nHovers > 10 ? 1 : nHovers / 10}`
+}
+
+function createRandomColorValue() {
+  const randomHue = Math.floor(Math.random() * 360);
+  return `hsl(${randomHue}deg 100% 50%)`;
+}
+
 function resetGrid(gridSize) {
+  resetHoverCount();
   mainContainer.replaceChildren();
   for (let i = 0; i < gridSize * gridSize; ++i) {
     const gridCell = document.createElement("div");
@@ -32,7 +52,10 @@ function resetGrid(gridSize) {
   }
 
   function activateGridCell(event) {
+    incrementHoverCount();
     event.target.classList.add("grid-cell--activated");
+    event.target.style.backgroundColor = createRandomColorValue();
+    event.target.style.opacity = getOpacityValue();
   }
 }
 
